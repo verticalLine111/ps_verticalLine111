4b12ca4c99d73bc12c44bd92109affbc397d905c
WooJongSeon <jswoo030@gmail.com>
2026-01-19 17:15:45 +0900

    two sum

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    prevMap = {}
+    for i in range(0, len(nums)):
+        diff = target - nums[i]
+        if diff in prevMap:
+            return [ prevMap[diff] , i ]
+        else:
+            prevMap[nums[i]] = i
+    
