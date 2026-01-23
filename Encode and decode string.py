e8c50ffd96d1cd04e1f252b4ff926c08cf1c90ec
WooJongSeon <jswoo030@gmail.com>
2026-01-23 14:51:59 +0900

    encode , decode

--- Encode and decode string.py
+++ Encode and decode string.py
@@ -1,19 +1,19 @@
-def encode( strs: list[str]) -> str:
-    if len(strs) == 0:
-        return ';'
-
-    for i in strs:
-
+def encode(strs: list[str]) -> str:
+    encodeStr= ''
+    for i in range(len(strs)):
+        encodeStr += str(len(strs[i])) + '#' + strs[i]
+    return encodeStr
     
-    return ";".join(strs)
-
-def decode( s: str) -> list[str]:
-    return s.split(';')
-
-dummy_input=[]
-print(encode(strs=dummy_input))
-print(decode(encode(strs=dummy_input)))
-
-# 타입이 바뀌는게 문제
-# 분리된 여러 스트링 리스트를 하나의 스트링으로 잠깐 합쳤다가
-# 다시 그걸 분리해서 출력하는 것
+def decode(s: str) -> list[str]:
+    res = []
+    i = 0
+    while i < len(s):
+        j = i
+        while s[j] != '#':
+            j+= 1
+        length = int(str(s[i:j]))
+        i = j+1
+        j = i + length
+        res.append(s[i:j])
+        i = j
+    return res
