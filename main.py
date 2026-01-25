dbad8d1318e902dd7561594328f0bda0d8231249
WooJongSeon <jswoo030@gmail.com>
2026-01-26 01:04:31 +0900

    two sum 2 input array sorted

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import _2_valid_anagram
 import _3_two_sum
 import _4_group_anagram
 import _8_valid_sudoku
-import _9_longest_consecutive_sequence
+import _11_two_sum_2_input_array_is_sorted
 
 if __name__ == "__main__":
-    sol = _9_longest_consecutive_sequence.Solution()
+    sol = _11_two_sum_2_input_array_is_sorted.Solution()
+    numbers = [2, 3, 4]
+    target = 6
 
-    nums = [2, 20, 4, 10, 3, 4, 5]
-
-    result = sol.longestConsecutive(nums)
+    result = sol.twoSum(numbers, target)
     print(f"결과: {result}")
