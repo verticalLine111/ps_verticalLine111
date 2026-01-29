e1febb0729cb32d51b7ef8452f5154a376e6cf9b
WooJongSeon <jswoo030@gmail.com>
2026-01-29 19:16:36 +0900

    two sum 2 input array is sorted

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ import _12_3sum
 import _10_valid_palindrome
 import _15_best_time_to_buy_and_sell_stock
 import _13_container_with_most_water
-import _9_longest_consecutive_sequence
+import _11_two_sum_2_input_array_is_sorted
 
 if __name__ == "__main__":
-    sol = _9_longest_consecutive_sequence.Solution()
-    result = sol.longestConsecutive([2,20,4,10,3,4,5])
+    sol = _11_two_sum_2_input_array_is_sorted.Solution()
+    numbers = [2, 3, 4]
+    target = 6
+    result = sol.twoSum(numbers, target)
 
     print(f"결과: {result}")
