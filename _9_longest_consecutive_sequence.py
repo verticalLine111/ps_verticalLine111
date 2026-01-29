6b790b765beecd0b4a2c9be6b4163ceb6cafbde5
WooJongSeon <jswoo030@gmail.com>
2026-01-29 15:16:19 +0900

    longest consecutive sequence

--- _9_longest_consecutive_sequence.py
+++ _9_longest_consecutive_sequence.py
@@ -1,13 +1,14 @@
 from typing import List
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         numSet = set(nums)
         longest = 0
-        for num in numSet:
-            if (num -1) not in numSet:
-                length = 0
-                while (num + length) in numSet:
+        for i, a in enumerate(numSet):
+            if (a-1) in numSet:
+                continue
+            else:
+                length=1
+                while (a + length) in numSet:
                     length += 1
                 longest = max(longest , length)
 
--- main.py
+++ main.py
@@ -8,19 +8,10 @@ import _12_3sum
 import _10_valid_palindrome
 import _15_best_time_to_buy_and_sell_stock
 import _13_container_with_most_water
+import _9_longest_consecutive_sequence
 
 if __name__ == "__main__":
-    sol = _8_valid_sudoku.Solution()
-    result = sol.isValidSudoku([
-        ["1","2",".",".","3",".",".",".","."],
-        ["4",".",".","5",".",".",".",".","."],
-        [".","9","8",".",".",".",".",".","3"],
-        ["5",".",".",".","6",".",".",".","4"],
-        [".",".",".","8",".","3",".",".","5"],
-        ["7",".",".",".","2",".",".",".","6"],
-        [".",".",".",".",".",".","2",".","."],
-        [".",".",".","4","1","9",".",".","8"],
-        [".",".",".",".","8",".",".","7","9"]
-    ])
+    sol = _9_longest_consecutive_sequence.Solution()
+    result = sol.longestConsecutive([2,20,4,10,3,4,5])
 
     print(f"결과: {result}")
