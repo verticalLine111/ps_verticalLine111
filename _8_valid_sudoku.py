1f86075ce2ad59409570e5c738788e0ec0f0a3bc
WooJongSeon <jswoo030@gmail.com>
2026-01-29 14:56:29 +0900

    valid sudoku

--- _8_valid_sudoku.py
+++ _8_valid_sudoku.py
@@ -3,19 +3,19 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        squares = defaultdict(set)
         rows = defaultdict(set)
         cols = defaultdict(set)
-        squares = defaultdict(set)
         for r in range(len(board)):
             for c in range(len(board)):
-                if board[r][c] == '.':
+                if board[r][c] =='.':
                     continue
                 if (board[r][c] in rows[r] or
-                    board[r][c] in cols[c] or
-                    board[r][c] in squares[(r //3 , c // 3)]):
+                        board[r][c] in cols[c] or
+                        board[r][c] in squares[(r // 3 , c // 3)]):
                     return False
                 else:
-                    cols[c].add(board[r][c])
                     rows[r].add(board[r][c])
-                    squares[(r // 3, c // 3)].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    squares[(r//3 , c//3)].add(board[r][c])
         return True
