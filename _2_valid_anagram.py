9b5e0d508cfe1b66e6963059d246b62ee2a6391e
WooJongSeon <jswoo030@gmail.com>
2026-01-31 19:08:19 +0900

    is anagram

--- _2_valid_anagram.py
+++ _2_valid_anagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        alpha = [0] * 26
+        for c in s:
+            alpha[ord(c) - ord('a')] += 1
+        for c in t:
+            alpha[ord(c) - ord('a')] -= 1
+        for v in alpha:
+            if v != 0:
+                return False
         return True
