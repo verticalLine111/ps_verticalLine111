8bf4000336e20cdc3edee0cf7afdc570a53e9297
WooJongSeon <jswoo030@gmail.com>
2026-02-11 22:28:32 +0900

    encode decode

--- _6_encode_and_decode_strings.py
+++ _6_encode_and_decode_strings.py
@@ -7,14 +7,14 @@ class Solution:
         return r
 
     def decode(self, s: str) -> List[str]:
-        i = 0
         res = []
+        i = 0
         while i < len(s):
-            j = i + 1
-
+            j = i
             while s[j] != '#':
-                j+=1
+                j += 1
             length = int(s[i:j])
+
             res.append(s[j + 1: j + 1 + length])
             i = j + 1 + length
         return res
