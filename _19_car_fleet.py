b33007b7c9a76d204babbca7ddfbd8953ce97587
WooJongSeon <jswoo030@gmail.com>
2026-02-12 21:17:52 +0900

    car fleet

--- _19_car_fleet.py
+++ _19_car_fleet.py
@@ -1,11 +1,12 @@
 from typing import List
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
         ziplist = list(zip(position, speed))
         ziplist.sort(reverse=True)
-        stack = []
         for p, s in ziplist:
-            stack.append((target - p) / s)
-            if len(stack) >=2 and stack[-1] <= stack[-2]:
+            time = (target - p) / s
+            stack.append(time)
+            while len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
