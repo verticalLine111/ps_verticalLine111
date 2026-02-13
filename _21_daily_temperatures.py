e13f3db9182df2668227fb82d58a35f209c64949
WooJongSeon <jswoo030@gmail.com>
2026-02-13 23:15:33 +0900

    daily temperature

--- _21_daily_temperatures.py
+++ _21_daily_temperatures.py
@@ -1,11 +1,11 @@
 from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []
+        stack =[]
         res = [0] * len(temperatures)
-        for i, t in enumerate(temperatures):
-            while stack and stack[-1][0] < t:
-                tmp , idx = stack.pop()
-                res[idx] = i - idx
-            stack.append((t, i))
+        for i,t in enumerate(temperatures):
+            while stack and stack[-1][1] < t:
+                sti, stt = stack.pop()
+                res[sti] = i - sti
+            stack.append((i , t))
         return res
