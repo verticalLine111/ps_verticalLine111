d7f8ad41df6516af190e67271989ac6fe69d3e09
WooJongSeon <jswoo030@gmail.com>
2026-02-25 17:43:35 +0900

    find min

--- _22_Find_Minimum_in_Rotated_Sorted_Array.py
+++ _22_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -1,7 +1,8 @@
 from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l,r = 0, len(nums) - 1
+        l = 0
+        r = len(nums) - 1
         while l < r:
             mid = (l + r) // 2
             if nums[mid] > nums[r]:
