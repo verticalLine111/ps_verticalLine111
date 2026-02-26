3baca776c4455c5d1cbbc7d3bef59802c76f185a
WooJongSeon <jswoo030@gmail.com>
2026-02-26 18:48:51 +0900

    longest character replacement

--- _26_Longest_Repeating_Character_Replacement.py
+++ _26_Longest_Repeating_Character_Replacement.py
@@ -1,18 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        fmap = {}
-        maxf = 0
-        res = 0
         l = 0
+        fmap = {}
+        res = 0
+        maxf = 0
         for r in range(len(s)):
             if s[r] in fmap:
                 fmap[s[r]] += 1
             else:
                 fmap[s[r]] = 1
-            maxf = max(maxf , fmap[s[r]])
-
-            while r - l + 1 - maxf  > k:
+            maxf = max(maxf, fmap[s[r]])
+            while r - l + 1 - maxf > k:
                 fmap[s[l]] -= 1
-                l+=1
-            res = max(res , r- l + 1 )
+                l += 1
+            res = max(res , r - l + 1)
         return res
