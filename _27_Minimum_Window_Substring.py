a53b8e71265e9d54d830c97adf3f10bcc17173e8
WooJongSeon <jswoo030@gmail.com>
2026-03-04 15:45:10 +0900

    minimum window substring

--- _27_Minimum_Window_Substring.py
+++ _27_Minimum_Window_Substring.py
@@ -4,23 +4,22 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         tmap = Counter(t)
         smap = Counter()
-        res = s + '!'
+        need = len(tmap)
+        have = 0
         l = 0
-        have, need = 0, len(tmap)
-
+        res = s + '!'
         for r in range(len(s)):
             smap[s[r]] += 1
-            if s[r] in tmap and tmap[s[r]] == smap[s[r]]:
+            if s[r] in smap and smap[s[r]] == tmap[s[r]]:
                 have += 1
 
             while have == need:
                 current = s[l:r + 1]
                 res = min(res, current, key=len)
-
                 smap[s[l]] -= 1
                 if s[l] in smap and smap[s[l]] < tmap[s[l]]:
                     have -= 1
-                l+=1
+                l += 1
 
         if len(res) > len(s):
             return ""
