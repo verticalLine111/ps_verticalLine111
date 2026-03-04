bdfa7dee31621d96b40a52d566465568f4f9ed46
WooJongSeon <jswoo030@gmail.com>
2026-03-04 13:51:36 +0900

    two su

--- _3_two_sum.py
+++ _3_two_sum.py
@@ -1,11 +1,10 @@
-from typing import List
+from typing import List, Counter
 
-def twoSum(nums: List[int], target: int) -> List[int]:
-    prevMap = {}
-    for i in range(0, len(nums)):
-        diff = target - nums[i]
-        if diff in prevMap:
-            return [ prevMap[diff] , i ]
-        else:
-            prevMap[nums[i]] = i
-    
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nmap = Counter()
+        for i in range(len(nums)):
+            if target - nums[i] in nmap:
+                return [nmap[target - nums[i]], i]
+            else:
+                nmap[nums[i]] = i
