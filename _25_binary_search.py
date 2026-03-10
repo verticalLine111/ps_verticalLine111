6e237b614a296588c321ba002d16fd655aadce5f
WooJongSeon <jswoo030@gmail.com>
2026-03-10 22:56:23 +0900

    binary search

--- _25_binary_search.py
+++ _25_binary_search.py
@@ -1,14 +1,13 @@
 from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums) - 1
-        while l <= r:
+        l,r = 0, len(nums)
+        while l < r:
             mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid] > target:
-                r = mid -1
-            else:
+            elif nums[mid] < target:
                 l = mid + 1
+            else:
+                r = mid
         return -1
