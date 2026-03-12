3429d67cc6608a1aee40460602c720f32a1968a2
WooJongSeon <jswoo030@gmail.com>
2026-03-12 23:01:10 +0900

    search in rotated sorted array

--- _23_Search_in_Rotated_Sorted_Array.py
+++ _23_Search_in_Rotated_Sorted_Array.py
@@ -5,18 +5,17 @@ class Solution:
         r = len(nums) - 1
         while l <= r:
             mid = (l + r) // 2
-
             if nums[mid] == target:
                 return mid
-
             if nums[mid] > nums[r]:
-                if nums[l] <= target < nums[mid]:
-                    r = mid -1
+                if nums[l] <= target and target < nums[mid]:
+                    r = mid - 1
                 else:
                     l = mid + 1
-            else:
-                if nums[mid] < target <= nums[r]:
+            else: 
+                if nums[mid] < target and target <= nums[r]:
                     l = mid + 1
                 else:
-                    r = mid -1
+                    r = mid - 1
+
         return -1
