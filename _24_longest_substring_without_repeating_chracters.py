80a3bd2c6ef2f6b76f180299f2111868f3bdb9b9
WooJongSeon <jswoo030@gmail.com>
2026-03-14 19:41:49 +0900

    lengthOfLongestSubstring

--- _24_longest_substring_without_repeating_chracters.py
+++ _24_longest_substring_without_repeating_chracters.py
@@ -1,14 +1,15 @@
-from collections import Counter
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        l = 0
-        smap = Counter()
+        smap = {}
         res = 0
+        l = 0
         for r in range(len(s)):
-            smap[s[r]] += 1
+            if s[r] in smap:
+                smap[s[r]] += 1
+            else:
+                smap[s[r]] = 1
             while smap[s[r]] > 1:
                 smap[s[l]] -= 1
-                l+= 1
-            res = max(res, r - l + 1)
+                l += 1
+            res = max(res ,r-l+1)
         return res
