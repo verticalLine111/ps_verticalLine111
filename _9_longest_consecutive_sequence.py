68bc612d3627b63bf92d5fa09b56e0bd6826c076
WooJongSeon <jswoo030@gmail.com>
2026-03-16 00:49:12 +0900

    longest consecutive sequence

--- _9_longest_consecutive_sequence.py
+++ _9_longest_consecutive_sequence.py
@@ -1,15 +1,13 @@
+from collections import Counter
 from typing import List
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numSet = set(nums)
-        longest = 0
-        for i, a in enumerate(numSet):
-            if (a-1) in numSet:
-                continue
-            else:
-                length=1
-                while (a + length) in numSet:
-                    length += 1
-                longest = max(longest , length)
-
-        return longest
+        nmap = Counter(nums)
+        res = 0
+        for n in nums:
+            l = 1
+            while n + l in nmap:
+                l+= 1
+            res = max(res, l)
+        return res
