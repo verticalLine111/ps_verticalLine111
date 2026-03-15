1395de7a1b01ee20e69f56be955e02ff4953a121
WooJongSeon <jswoo030@gmail.com>
2026-03-15 23:32:26 +0900

    longest repeating character replacement

--- _26_Longest_Repeating_Character_Replacement.py
+++ _26_Longest_Repeating_Character_Replacement.py
@@ -3,15 +3,18 @@ from collections import Counter
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         smap = Counter()
-        l = 0
-        res = 0
         maxf = 0
+        res = 0
+        l = 0
         for r in range(len(s)):
-            smap[s[r]] += 1
-            maxf = max(maxf , smap[s[r]])
+            if s[r] in smap:
+                smap[s[r]] += 1
+            else:
+                smap[s[r]] = 1
+            maxf = max(maxf, smap[s[r]])
+
             while r - l + 1 - maxf > k:
                 smap[s[l]] -= 1
                 l += 1
-            res = max(res, len(s[l:r + 1]))
-
+            res = max(res, r - l + 1)
         return res
