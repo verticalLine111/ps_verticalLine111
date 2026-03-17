b40fd2192712b56f8c64dba925ad1fc5736754ac
WooJongSeon <jswoo030@gmail.com>
2026-03-17 20:04:51 +0900

    group anagrams

--- _4_group_anagram.py
+++ _4_group_anagram.py
@@ -1,11 +1,17 @@
 from collections import defaultdict
 from typing import List
+
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         amap = defaultdict(list)
-        for s in strs:
+        for s in range(len(strs)):
             alpha = [0] * 26
-            for c in s:
+            for c in strs[s]:
                 alpha[ord(c) - ord('a')] += 1
-            amap[tuple(alpha)].append(s)
-        return list(amap.values())
+            amap[tuple(alpha)].append(strs[s])
+        res = []
+        for k, v in amap.items():
+            res.append(v)
+        return res
+
