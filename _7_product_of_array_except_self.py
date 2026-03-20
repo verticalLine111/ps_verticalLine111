d21b1586c2637f21d02c824a796ac32da2536625
WooJongSeon <jswoo030@gmail.com>
2026-03-20 23:28:22 +0900

    product except

--- _7_product_of_array_except_self.py
+++ _7_product_of_array_except_self.py
@@ -3,14 +3,13 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
-
-        prefix = 1
+        res = [1] * len(nums)
+        pref = 1
         for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
+            res[i] = pref
+            pref *= nums[i]
+        post = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= post
+            post *= nums[i]
         return res
