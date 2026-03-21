a910c1ac49e21d16a7390801ee8a56782f3ff5fb
WooJongSeon <jswoo030@gmail.com>
2026-03-22 01:08:25 +0900

    3sum

--- _12_3sum.py
+++ _12_3sum.py
@@ -1,25 +1,25 @@
 from typing import List
+
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
-        for i ,a in enumerate(nums):
-            if a > 0:
-                break
-            if i > 0 and a == nums[i-1]:
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-
             l = i+1
-            r = len(nums) - 1
+            r = len(nums) -1
             while l < r:
-                sum = nums[i] + nums[l] + nums[r]
-                if sum > 0:
+                total = nums[i] + nums[l] + nums[r]
+                if total < 0:
+                    l+=1
+                elif total > 0:
                     r -= 1
-                elif sum < 0:
-                    l+=1
                 else:
-                    res.append([a, nums[l], nums[r]])
+                    res.append([nums[i] , nums[l] , nums[r]])
                     l+=1
-                    while l > 0 and nums[l] == nums[l-1] and  l<r:
+                    r-=1
+                    while l < r and nums[l] == nums[l-1]:
                         l+=1
         return res
