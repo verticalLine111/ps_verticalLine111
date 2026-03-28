f3cf7f92019dc89081f3c8ca8649584e9714e84a
WooJongSeon <jswoo030@gmail.com>
2026-03-28 23:18:23 +0900

    merge sorted linked list

--- _29_Merge_Two_Sorted_Linked_Lists.py
+++ _29_Merge_Two_Sorted_Linked_Lists.py
@@ -9,16 +9,14 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = node = ListNode()
-
+        first = temp = ListNode()
         while list1 and list2:
             if list1.val > list2.val:
-                node.next = list2
+                temp.next = list2
                 list2 = list2.next
             else:
-                node.next = list1
+                temp.next = list1
                 list1 = list1.next
-            node = node.next
-
-        node.next = list1 or list2
-        return dummy.next
+            temp = temp.next
+        temp.next = list1 or list2
+        return first.next
