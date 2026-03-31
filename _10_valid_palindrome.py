a8afc298432b558a39933a9ba764929a67ffd19d
WooJongSeon <jswoo030@gmail.com>
2026-03-31 21:25:10 +0900

    valid palindrome

--- _10_valid_palindrome.py
+++ _10_valid_palindrome.py
@@ -1,23 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         l = 0
-        r = len(s) - 1
-        lower = s.lower()
-        while l <= r:
-            if self.isAN(lower[l]) and self.isAN(lower[r]):
-                if lower[l] != lower[r]:
+        r = len(s)-1
+        while l < r:
+            if self.isAN(s[l].lower()) and self.isAN(s[r].lower()):
+                if s[l].lower() != s[r].lower():
                     return False
                 else:
                     l += 1
                     r -= 1
             else:
-                if not self.isAN(lower[l]):
-                    l += 1
-                if not self.isAN(lower[r]):
-                    r -= 1
+                if not self.isAN(s[l].lower()):
+                    l+=1
+                if not self.isAN(s[r].lower()):
+                    r-=1
         return True
 
-    def isAN(self, c:str) -> bool:
+    def isAN(self, c: str) -> bool:
         if ord('a') <= ord(c) <= ord('z'):
             return True
         if '0' <= c <= '9':
