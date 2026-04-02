3cab5b40a960874f6a49475539b1ff33e7ae0577
verticalLine111 <jswoo030@gmail.com>
2026-04-02 15:06:55 +0900

    Add: minimum-stack - submission-12

---
+++ Data Structures & Algorithms/minimum-stack/submission-12.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stk = []
+        self.minstk = []
+
+    def push(self, val: int) -> None:
+        if not self.minstk:
+            self.minstk.append(val)
+            self.stk.append(val)
+        else:
+            if self.minstk[-1] > val:
+                self.minstk.append(val)
+            else:
+                self.minstk.append(self.minstk[-1])
+            self.stk.append(val)
+
+    def pop(self) -> None:
+        self.stk.pop()
+        self.minstk.pop()
+
+    def top(self) -> int:
+        return self.stk[-1]
+
+    def getMin(self) -> int:
+        return self.minstk[-1]
